803c248878e38d53306983f50e396253a076f39d
Hussein-Elshinnawy <clorkthefirst@gmail.com>
2024-09-05 19:45:57 +0300

    authorization for candidate and company

--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,29 @@ use App\Http\Controllers\CandidateController;
 use App\Http\Controllers\CompanyController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\CheckUserType;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 
-Route::middleware(['auth'])->group(function () {
+// Route::middleware(['auth'])->group(function () {
 
+//     Route::get('/candidate/profile', [CandidateController::class, 'index'])->name('candidate.profile');
+
+//     Route::get('/company/profile', [CompanyController::class, 'index'])->name('company.profile');
+// });
+
+
+Route::middleware(['auth', 'checkUserType:candidate'])->group(function () {
     Route::get('/candidate/profile', [CandidateController::class, 'index'])->name('candidate.profile');
+});
 
+Route::middleware(['auth', 'checkUserType:company'])->group(function () {
     Route::get('/company/profile', [CompanyController::class, 'index'])->name('company.profile');
 });
+
 Route::resource('candidate', CandidateController::class);
 Route::resource('company', CompanyController::class);
 Auth::routes();
